a7493dacfe831af223a41aba7009911df323d92a
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2024-07-06 20:53:05 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,36 @@
-import globals from "globals";
+import globals from 'globals'
 
-export default [{
-    languageOptions: {
-        globals: {
-            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
-            ...globals.commonjs,
-            ...globals.node,
-            ...globals.mocha,
-        },
+export default [
+	{
+		languageOptions: {
+			globals: {
+				...Object.fromEntries(
+					Object.entries(globals.browser).map(([key]) => [key, 'off'])
+				),
+				...globals.commonjs,
+				...globals.node,
+				...globals.mocha,
+			},
 
-        ecmaVersion: 2018,
-        sourceType: "module",
+			ecmaVersion: 2018,
+			sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
-    },
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
 
-    rules: {
-        "no-const-assign": "warn",
-        "no-this-before-super": "warn",
-        "no-undef": "warn",
-        "no-unreachable": "warn",
-        "no-unused-vars": "warn",
-        "constructor-super": "warn",
-        "valid-typeof": "warn",
-    },
-}];
+		rules: {
+			'no-const-assign': 'warn',
+			'no-this-before-super': 'warn',
+			'no-undef': 'warn',
+			'no-unreachable': 'warn',
+			'no-unused-vars': 'warn',
+			'constructor-super': 'warn',
+			'valid-typeof': 'warn',
+		},
+		ignores: ['out', 'dist', '**/*.d.ts', '.vscode-test/'],
+	},
+]
