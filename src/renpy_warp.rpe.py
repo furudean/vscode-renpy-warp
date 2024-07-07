c1825cb84c15d87e9c789cd6e67a84176c4ce678
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2024-07-07 17:05:08 +0200

    don't try and run code if in init phase

--- src/renpy_warp.rpe.py
+++ src/renpy_warp.rpe.py
@@ -20,6 +20,10 @@ port = os.getenv("WARP_WS_PORT")
 
 
 def py_exec(text: str):
+    while renpy.exports.is_init_phase():
+        print("in init phase, waiting...")
+        sleep(0.5)
+
     fn = functools.partial(renpy.python.py_exec, text)
     renpy.exports.invoke_in_main_thread(fn)
 
