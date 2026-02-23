0aa45021e62a37f1da1de31ab826bf73d759e526
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2026-02-23 14:37:01 -0800

    update release command

--- release.sh
+++ release.sh
@@ -34,19 +34,24 @@ if [[ ! -f "CHANGELOG.md" ]]; then
   exit 1
 fi
 
-# bump version via npm
-npm version "$BUMP_TYPE" --no-git-tag-version > /dev/null
-
-# replace ## Unreleased in CHANGELOG.md
 if ! grep -q "^## Unreleased" CHANGELOG.md; then
   echo "error: could not find '## Unreleased' in CHANGELOG.md"
   exit 1
 fi
 
+
+# bump version via npm
+npm version "$BUMP_TYPE" --no-git-tag-version > /dev/null
+
 # read the new version
 NEW_VERSION=$(jq -r '.version' package.json)
 TODAY=$(date +"%Y-%m-%d")
 
+# extract changelog body
+NOTES_FILE=$(mktemp)
+awk '/^## Unreleased/{found=1; next} found && /^## /{exit} found' CHANGELOG.md > "$NOTES_FILE"
+
+# update changelog with release version and date
 perl -i -pe "s/^## Unreleased/## ${NEW_VERSION} - ${TODAY}/" CHANGELOG.md
 
 # commit
@@ -55,14 +60,13 @@ git commit -m "release v${NEW_VERSION}"
 
 # create a git tag
 git tag "v${NEW_VERSION}"
-
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
 echo ""
 echo "made commit for v${NEW_VERSION}"
 echo ""
-echo "to push commit and tag, run:"
-echo "  git push && git push origin v${NEW_VERSION}"
+echo "to push commit, tag, and create github release, run:"
+echo "  git push && git push origin v${NEW_VERSION} && gh release create v${NEW_VERSION} --notes-file \"${NOTES_FILE}\""
 echo ""
 echo "to undo commit and remove the tag, run:"
 echo "  git tag -d v${NEW_VERSION} && git reset ${COMMIT_HASH}^"
